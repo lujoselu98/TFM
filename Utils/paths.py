48db0d7cb6182ec2958c00e255c41e4678360db9
Jose Luis <jllavado0@gmail.com>
2022-01-27 15:33:48 +0100

    Minor fix in paths

--- Utils/paths.py
+++ Utils/paths.py
@@ -3,15 +3,13 @@
 """
 
 # Paths for Data
-DATA_PATH = "/Data"
-ORIGINAL_DATA_PATH = f"{DATA_PATH}/Original_Data"
+DATA_PATH: str = "/Data"
+ORIGINAL_DATA_PATH: str = f"{DATA_PATH}/Original_Data"
 
 
 def _print_paths() -> None:
     """
     Function just to print all paths
-
-    :return:
     """
     print(f"{DATA_PATH=}")
     print(f"{ORIGINAL_DATA_PATH=}")
