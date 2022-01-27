60ee2a7275173883c06baf318208f7ceb5a8b214
Jose Luis <jllavado0@gmail.com>
2022-01-27 18:46:32 +0100

    Integrity check

--- Utils/paths.py
+++ Utils/paths.py
@@ -1,9 +1,15 @@
 """
     This file is just for defining path constants
 """
+import os
+
+# Root of project get the parent folder
+PROJECT_PATH: str = os.path.abspath(os.path.dirname(__file__))
+PROJECT_PATH = '/'.join(PROJECT_PATH.split('\\')[:-1])
+
 
 # Paths for Data
-DATA_PATH: str = "/Data"
+DATA_PATH: str = f"{PROJECT_PATH}/Data"
 ORIGINAL_DATA_PATH: str = f"{DATA_PATH}/Original_Data"
 
 
@@ -11,9 +17,13 @@ def _print_paths() -> None:
     """
     Function just to print all paths
     """
+    print(f"{PROJECT_PATH=}")
+    print()
+
     print(f"{DATA_PATH=}")
     print(f"{ORIGINAL_DATA_PATH=}")
 
 
 if __name__ == '__main__':
     _print_paths()
+    print()
