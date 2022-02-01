5da3d6ec8fa84d9cbb5f2e9203529af4264c82a4
Jose Luis <jllavado0@gmail.com>
2022-02-01 11:23:57 +0100

    Elimination of non usefull curves for prediction

--- Utils/pandas_utils.py
+++ Utils/pandas_utils.py
@@ -4,14 +4,16 @@
 import numpy as np
 
 
-def constant_size(signal_values: np.array) -> int:
+def constant_size(signal_values: np.array, threshold: int = 1) -> int:
     """
 
     Calculate the total size of the constant part of the signal
 
+    :param threshold: param to set which size of the signal parts take into account
     :param signal_values: signal to calculate constant part size
     :return: the len of the sum of all constant parts on signal
     """
+    # noinspection PyTypeChecker
     return np.sum([x.size
                    for x in np.split(signal_values, np.where(np.diff(signal_values) != 0)[0] + 1)
-                   if x.size > 1])[0]
+                   if x.size > threshold])
