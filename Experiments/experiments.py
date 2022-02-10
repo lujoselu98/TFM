6117a9ce794c6624bfcf9d3b6ce7e92f78876e11
Jose Luis <jllavado0@gmail.com>
2022-02-10 14:12:04 +0100

    Fixed results to use in experiments

--- Experiments/experiments.py
+++ Experiments/experiments.py
@@ -1,3 +1,15 @@
 """
-    Main File to do Differents Experiments and save the resulst
+    Main File to do Different Experiments and save the results
 """
+from tqdm import tqdm
+
+from Utils import fixed_values
+
+
+def main_experiment() -> None:
+    """Function to made the main experiment"""
+
+    progress_bar = tqdm(fixed_values.DATASETS,
+                        total=len(fixed_values.DATASETS) * len(fixed_values.CLASSIFIERS) * len(
+                            fixed_values.PREPROCESSES) * fixed_values.EXTERNAL_SPLITS)
+
--- Utils/fixed_values.py
+++ Utils/fixed_values.py
@@ -2,6 +2,14 @@
     Common shared values
 """
 
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import balanced_accuracy_score, roc_auc_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.svm import SVC
+
 DATASETS = ['CC', 'DCOR', 'FFT']
 
 EXTERNAL_SPLITS = 10
@@ -11,6 +19,94 @@ PREPROCESSES = ['mRMR', 'PCA', 'PLS']
 
 DIMENSION_GRID = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 30, 40, 50, 75, 100]
 MAX_DIMENSION = max(DIMENSION_GRID)
+
+CLASSIFIERS = {
+    'LR': {
+        'clf': LogisticRegression(penalty='l1', solver='liblinear', random_state=0, class_weight='balanced',
+                                  max_iter=1000),
+        'param_grid': {
+            'C': np.logspace(-3, 2, 6)
+        },
+        'evaluate_score': 'predict_proba',
+    },
+    'LRSScaler': {
+        'clf': make_pipeline(StandardScaler(), LogisticRegression(penalty='l1', solver='liblinear', random_state=0,
+                                                                  class_weight='balanced',
+                                                                  max_iter=1000)),
+        'param_grid': {
+            'logisticregression__C': np.logspace(-3, 2, 6)
+        },
+        'evaluate_score': 'predict_proba',
+    },
+    'LRmMScaler': {
+        'clf': make_pipeline(MinMaxScaler(), LogisticRegression(penalty='l1', solver='liblinear', random_state=0,
+                                                                class_weight='balanced',
+                                                                max_iter=1000)),
+        'param_grid': {
+            'logisticregression__C': np.logspace(-3, 2, 6)
+        },
+        'evaluate_score': 'predict_proba',
+    },
+    'KNN': {
+        'clf': KNeighborsClassifier(),
+        'param_grid': {
+            'n_neighbors': np.arange(3, 30, 2),
+        },
+        'evaluate_score': 'predict_proba',
+    },
+    'KNNSScaler': {
+        'clf': make_pipeline(StandardScaler(), KNeighborsClassifier()),
+        'param_grid': {
+            'kneighborsclassifier__n_neighbors': np.arange(3, 30, 2),
+        },
+        'evaluate_score': 'predict_proba',
+    },
+    'KNNmMScaler': {
+        'clf': make_pipeline(MinMaxScaler(), KNeighborsClassifier()),
+        'param_grid': {
+            'kneighborsclassifier__n_neighbors': np.arange(3, 30, 2),
+        },
+        'evaluate_score': 'predict_proba',
+    },
+    'SVC': {
+        'clf': SVC(kernel='rbf', random_state=0, class_weight='balanced'),
+        'param_grid': {
+            'gamma': np.logspace(-3, 3, 7),
+            'C': np.logspace(-3, 3, 7),
+        },
+        'evaluate_score': 'decision_function',
+    },
+    'SVCSScaler': {
+        'clf': make_pipeline(StandardScaler(), SVC(kernel='rbf', random_state=0, class_weight='balanced')),
+        'param_grid': {
+            'svc__gamma': np.logspace(-3, 3, 7),
+            'svc__C': np.logspace(-3, 3, 7),
+        },
+        'evaluate_score': 'decision_function',
+    },
+    'SVCmMScaler': {
+        'clf': make_pipeline(MinMaxScaler(), SVC(kernel='rbf', random_state=0, class_weight='balanced')),
+        'param_grid': {
+            'svc__gamma': np.logspace(-3, 3, 7),
+            'svc__C': np.logspace(-3, 3, 7),
+        },
+        'evaluate_score': 'decision_function',
+    },
+}
+
+VALIDATION_METRIC = balanced_accuracy_score
+
+EVALUATION_METRICS = {
+    'AUC_SCORE': {
+        'function': roc_auc_score,
+        'values': 'scores'
+    },
+    'BAL_ACC': {
+        'function': balanced_accuracy_score,
+        'values': 'predictions'
+    },
+
+}
 
 
 def _print_values() -> None:
@@ -22,6 +118,10 @@ def _print_values() -> None:
     print(f"PREPROCESSES: {PREPROCESSES}")
     print(f"DIMENSION_GRID: {DIMENSION_GRID}")
     print(f"MAX_DIMENSION: {MAX_DIMENSION}")
+
+    print(f"CLASSIFIERS: {CLASSIFIERS}")
+    print(f"VALIDATION_METRIC: {VALIDATION_METRIC}")
+    print(f"EVALUATION_METRICS: {EVALUATION_METRICS}")
 
 
 if __name__ == '__main__':
